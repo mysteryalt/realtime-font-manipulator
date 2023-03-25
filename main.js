c67429abdc089e9305ad56d060327934cdf56b66
mysteryalt <86229694+mysteryalt@users.noreply.github.com>
2023-03-25 15:47:21 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
+leftWristX = 0;
+leftWristY = 0;
+rightWristX =0;
+rightWristY = 0;
+difference = 0;
 function setup() {
     video =  createCapture(VIDEO);
     video.size(550,500);
@@ -11,8 +16,11 @@ function setup() {
    }
 
    function draw() {
+    image(video , 0 , 0 , 550 , 500)
     background('grey')
-    
+    textSize(difference);
+    fill('black');
+    text("hello everyone , this is the font manipulator web app" , 50, 250)
     }
     
     function preload() {
@@ -28,5 +36,14 @@ function setup() {
     
     if (results.length > 0) {
         console.log(results);
+
+        leftWristX = results[0].pose.leftWrist.x;
+        leftWristY = results[0].pose.leftWrist.y;
+        rightWristX = results[0].pose.rightWrist.x;
+        rightWristY = results[0].pose.rightWrist.y;
+
+        difference = floor(leftWristX - rightWristX);
     }
+
+
     }
